8b09e46c0acae0d2dc423cddf76d91cffe893619
vahit <vahid.maani@gmail.com>
2017-11-18 16:48:01 +0330

    resolve some small mistake.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -11,14 +11,12 @@ mysqldump --user=$SOURCE_DB_USER --password=$SOURCE_DB_PASS --host=$SOURCE_DB_HO
 echo "------- exporting done."
 
 echo "------- trying to import DB"
-mysql --protocol=TCP --user=root --password=$MYSQL_ROOT_PASSWORD --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE jabeh;"
-mysql --protocol=TCP --user=root --password=$MYSQL_ROOT_PASSWORD --host=127.0.0.1 --port=3306 --database=jabeh < ${SOURCE_DB}-db.sql
+mysql --protocol=TCP --user=root --password=$MYSQL_ROOT_PASSWORD --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
+echo "------- ${SOURCE_DB} database created."
+mysql --protocol=TCP --user=root --password=$MYSQL_ROOT_PASSWORD --host=127.0.0.1 --port=3306 --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
 echo "------- importing done."
 
 rm ./${SOURCE_DB}-db.sql
 
-echo "------- killall mysqld"
-killall mysqld
-sleep 10
-echo "------- and run it agian"
-/usr/local/bin/docker-entrypoint.sh mysqld
+echo "------- wait until mysqld stop/crush ..."
+wait
