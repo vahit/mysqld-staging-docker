4803e259c2f1de0f8068d7dc10d41c6b3d721920
vahit <vahid.maani@gmail.com>
2017-12-18 16:15:19 +0330

    Now you can import only schema with "only_schema" environment

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,14 +6,19 @@ echo "------- start mysqld ..."
 sleep 30
 echo "------- mysqld: started."
 
-echo "------- export from production DB, it may be take some minutes ..."
-mysqldump --user=$SOURCE_DB_USER --password=$SOURCE_DB_PASS --host=$SOURCE_DB_HOST $SOURCE_DB --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+if [[ ${ONLY_SCHEMA} == "true" || ${ONLY_SCHEMA} == "TRUE" || ${ONLY_SCHEMA} == "True" ]]; then
+    echo "------- export from schematic of production DB, it may be take some minutes ..."
+    mysqldump --user=${SOURCE_DB_USER} --password=${SOURCE_DB_PASS} --host=${SOURCE_DB_HOST} --no-data --databases ${SOURCE_DB} --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+else
+    echo "------- export from production DB, it may be take some minutes ..."
+    mysqldump --user=${SOURCE_DB_USER} --password=${SOURCE_DB_PASS} --host=${SOURCE_DB_HOST} ${SOURCE_DB} --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+fi
 echo "------- exporting done."
 
 echo "------- trying to import DB"
-mysql --protocol=TCP --user=root --password=$MYSQL_ROOT_PASSWORD --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
+mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
 echo "------- ${SOURCE_DB} database created."
-mysql --protocol=TCP --user=root --password=$MYSQL_ROOT_PASSWORD --host=127.0.0.1 --port=3306 --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
+mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
 echo "------- importing done."
 
 rm ./${SOURCE_DB}-db.sql
