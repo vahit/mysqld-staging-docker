f928dd9eb2ace6fb7449bb67f4dfdeb170ba4707
vahit <vahid.maani@gmail.com>
2017-12-31 17:03:45 +0330

    add empty database creation option

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,22 +6,28 @@ echo "------- start mysqld ..."
 sleep 30
 echo "------- mysqld: started."
 
-if [[ ${ONLY_SCHEMA} == "true" || ${ONLY_SCHEMA} == "TRUE" || ${ONLY_SCHEMA} == "True" ]]; then
-    echo "------- export from schematic of production DB, it may be take some minutes ..."
-    mysqldump --user=${SOURCE_DB_USER} --password=${SOURCE_DB_PASS} --host=${SOURCE_DB_HOST} --no-data --databases ${SOURCE_DB} --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+if [[ ${EMPTY_DB} == "true" || ${EMPTY_DB} == "TRUE" || ${EMPTY_DB} == "True" ]]; then
+    echo "------- creating ${SOURCE_DB} DB"
+    mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
+    echo "------- ${SOURCE_DB} database created."
 else
-    echo "------- export from production DB, it may be take some minutes ..."
-    mysqldump --user=${SOURCE_DB_USER} --password=${SOURCE_DB_PASS} --host=${SOURCE_DB_HOST} ${SOURCE_DB} --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+    if [[ ${ONLY_SCHEMA} == "true" || ${ONLY_SCHEMA} == "TRUE" || ${ONLY_SCHEMA} == "True" ]]; then
+        echo "------- export from schematic of production DB, it may be take some minutes ..."
+        mysqldump --user=${SOURCE_DB_USER} --password=${SOURCE_DB_PASS} --host=${SOURCE_DB_HOST} --no-data --databases ${SOURCE_DB} --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+    else
+        echo "------- export from production DB, it may be take some minutes ..."
+        mysqldump --user=${SOURCE_DB_USER} --password=${SOURCE_DB_PASS} --host=${SOURCE_DB_HOST} ${SOURCE_DB} --skip-lock-tables --result-file=${SOURCE_DB}-db.sql
+    fi
+    echo "------- exporting done."
+
+    echo "------- creating ${SOURCE_DB} DB"
+    mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
+    echo "------- ${SOURCE_DB} database created."
+    mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
+    echo "------- ${SOURCE_DB} imported."
+
+    rm ./${SOURCE_DB}-db.sql
 fi
-echo "------- exporting done."
-
-echo "------- trying to import DB"
-mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
-echo "------- ${SOURCE_DB} database created."
-mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
-echo "------- importing done."
-
-rm ./${SOURCE_DB}-db.sql
 
 echo "------- wait until mysqld stop/crush ..."
 wait
