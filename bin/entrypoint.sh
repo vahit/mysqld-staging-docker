c264bd43c99c8f34517b05226960f760a9f150ee
vahit <vahid.maani@gmail.com>
2018-02-28 16:13:06 +0330

    add default values to variables

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -eo pipefail
 
+# defaults value for variables.
+EMPTY_DB=${EMPTY_DB:="TRUE"}
+MYSQL_PASSWORD=${MYSQL_ROOT_PASSWORD:="123456"}
+MYSQL_USER=${USER:="root"}
+HOST=${HOST:="127.0.0.1"}
+PORT=${PORT:="3306"}
+ONLY_SCHEMA=${ONLY_SCHEMA:="FALSE"}
+SOURCE_DB_USER=${SOURCE_DB_USER:="root"}
+SOURCE_DB_PASS=${SOURCE_DB_PASS:="123456"}
+SOURCE_DB=${SOURCE_DB:=""}
+SOURCE_DB_HOST=${SOURCE_DB_HOST:=""}
+DEFAULT_QUERIES=${DEFAULT_QUERIES:=""}
+
 echo "------- start mysqld ..."
 /usr/local/bin/docker-entrypoint.sh mysqld &
 sleep 30
@@ -8,7 +21,7 @@ echo "------- mysqld: started."
 
 if [[ ${EMPTY_DB} == "true" || ${EMPTY_DB} == "TRUE" || ${EMPTY_DB} == "True" ]]; then
     echo "------- creating ${SOURCE_DB} DB"
-    mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
+    mysql --protocol=TCP --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${HOST} --port=${PORT} --execute="CREATE DATABASE ${SOURCE_DB};"
     echo "------- ${SOURCE_DB} database created."
 else
     if [[ ${ONLY_SCHEMA} == "true" || ${ONLY_SCHEMA} == "TRUE" || ${ONLY_SCHEMA} == "True" ]]; then
@@ -21,9 +34,9 @@ else
     echo "------- exporting done."
 
     echo "------- creating ${SOURCE_DB} DB"
-    mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --execute="CREATE DATABASE ${SOURCE_DB};"
+    mysql --protocol=TCP --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${HOST} --port=${PORT} --execute="CREATE DATABASE ${SOURCE_DB};"
     echo "------- ${SOURCE_DB} database created."
-    mysql --protocol=TCP --user=root --password=${MYSQL_ROOT_PASSWORD} --host=127.0.0.1 --port=3306 --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
+    mysql --protocol=TCP --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=${HOST} --port=${PORT} --database="${SOURCE_DB}" < ${SOURCE_DB}-db.sql
     echo "------- ${SOURCE_DB} imported."
 
     rm ./${SOURCE_DB}-db.sql
